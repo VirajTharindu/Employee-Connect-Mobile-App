ca834c529128b50c8fb7e74981bf6e355f232b60
VirajBandara <96607866+VirajBandara@users.noreply.github.com>
2024-11-05 11:41:41 +0530

    3rd update

--- ios/Podfile.rb
+++ ios/Podfile.rb
@@ -1,12 +1,12 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '9.0'
 
 # CocoaPods analytics sends network stats synchronously affecting flutter build latency.
 # Disable CocoaPods analytics by adding the following line:
 # ENV['COCOAPODS_DISABLE_STATS'] = 'true'
 
 # You can uncomment the next line to define a global platform for your project
-platform :ios, '12.0'
+#platform :ios, '12.0'
 
 # The target is the name of your Flutter project
 target 'Runner' do
